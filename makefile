b5092abbf496349608de3dc3ac8e4cb464ce615b
Artem <ArtemGr@users.noreply.github.com>
2012-05-24 05:52:18 +0000

    gstring

--- makefile
+++ makefile
@@ -1,26 +1,61 @@
 
 PREFIX = /usr/local
 INSTALL2 = ${PREFIX}/include/glim
+CXXFLAGS = -std=c++0x -Wall -Ofast -ggdb
 
 all: test
 
 help:
 	@echo "make test\nmake install\nmake uninstall\nmake clean"
 
-test: test_sqlite
+test: test_sqlite test_gstring
 
 test_sqlite: bin/test_sqlite
 	cp bin/test_sqlite /tmp/libglim_test_sqlite && chmod +x /tmp/libglim_test_sqlite && /tmp/libglim_test_sqlite && rm -f /tmp/libglim_test_sqlite
 
 bin/test_sqlite: test_sqlite.cc
 	mkdir -p bin
-	g++ -std=c++0x -Wall -Ofast -g3 test_sqlite.cc -o bin/test_sqlite -lsqlite3
+	g++ $(CXXFLAGS) test_sqlite.cc -o bin/test_sqlite -lsqlite3
+
+test_couchbase: bin/test_couchbase
+	cp bin/test_couchbase /tmp/libglim_test_couchbase && chmod +x /tmp/libglim_test_couchbase && /tmp/libglim_test_couchbase && rm -f /tmp/libglim_test_couchbase
+
+test_memcache: bin/test_memcache
+	cp bin/test_memcache /tmp/libglim_test_memcache && chmod +x /tmp/libglim_test_memcache && /tmp/libglim_test_memcache && rm -f /tmp/libglim_test_memcache
+
+bin/test_couchbase: test_couchbase.cc couchbase.hpp couchbase.cpp
+	mkdir -p bin
+	g++ $(CXXFLAGS) test_couchbase.cc -o bin/test_couchbase -lcouchbase
+
+bin/test_memcache: test_memcache.cc memcache.hpp
+	mkdir -p bin
+	g++ $(CXXFLAGS) test_memcache.cc -o bin/test_memcache -lmemcache
+
+bin/test_gstring: test_gstring.cc gstring.hpp
+	mkdir -p bin
+	g++ $(CXXFLAGS) test_gstring.cc -o bin/test_gstring
+
+test_gstring: bin/test_gstring
+	cp bin/test_gstring /tmp/libglim_test_gstring
+	chmod +x /tmp/libglim_test_gstring
+	/tmp/libglim_test_gstring
+	rm -f /tmp/libglim_test_gstring
 
 install:
 	mkdir -p ${INSTALL2}/
 	cp sqlite.hpp ${INSTALL2}/
 	cp NsecTimer.hpp ${INSTALL2}/
 	cp TscTimer.hpp ${INSTALL2}/
+	cp memcache.hpp ${INSTALL2}/
+	cp gstring.hpp ${INSTALL2}/
+
+install_couchbase:
+	mkdir -p bin
+	g++ $(CXXFLAGS) -fpic -shared -o bin/libglimcouchbase.so couchbase.cpp -lcouchbase
+	rm -f /usr/local/lib/libglimcouchbase.so
+	cp bin/libglimcouchbase.so /usr/local/lib/
+	ldconfig
+	cp couchbase.hpp ${INSTALL2}/
 
 uninstall:
 	rm -f ${INSTALL2}/sqlite.hpp
@@ -28,4 +63,5 @@ uninstall:
 	rm -f ${INSTALL2}/TscTimer.hpp
 
 clean:
-	rm -rf bin/
+	rm -rf bin/*
+	rm -f /tmp/libglim_test_*
---
+++ test_gstring.cc
@@ -0,0 +1,63 @@
+
+#include "gstring.hpp"
+using glim::gstring;
+#include <assert.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+int main () {
+  std::cout << "Testing gstring.hpp ... " << std::flush;
+
+  gstring gs;
+  if (gs.needsFreeing()) throw std::runtime_error ("Default gstring needsFreeing");
+  if (gs.isReadOnly()) throw std::runtime_error ("Default gstring isReadOnly");
+  if (gs.capacity() != 1) throw std::runtime_error ("Default gstring capacity is not 1");
+  char buf16[16];
+  gstring gs16 (sizeof (buf16), buf16, false, false, 0);
+  if (gs16.capacity() != 16) throw std::runtime_error ("gs16 capacity != 16");
+  if (gs16.size() != 0) throw std::runtime_error ("gs16 size != 0");
+  gstring gsFree (17, NULL, true, false, 0);
+  if (!gsFree.needsFreeing()) throw std::runtime_error ("!needsFreeing");
+  if (gsFree.isReadOnly()) throw std::runtime_error ("isReadOnly");
+  if (gsFree.capacity() != 16) throw std::runtime_error ("gsFree capacity != 16");
+  if (gsFree.size() != 0) throw std::runtime_error ("gsFree size != 0");
+  gstring gsRO (31, NULL, false, true, 0);
+  if (gsRO.needsFreeing()) throw std::runtime_error ("needsFreeing");
+  if (!gsRO.isReadOnly()) throw std::runtime_error ("!isReadOnly");
+  if (gsRO.capacity() != 16) throw std::runtime_error ("gsRO capacity != 16");
+  if (gsRO.size() != 0) throw std::runtime_error ("gsRO size != 0");
+  char buf32[32];
+  gstring gs32 (sizeof (buf32), buf32, false, false, 0);
+  if (gs32.capacity() != 32) throw std::runtime_error ("capacity != 32");
+  if (gs32.size() != 0) throw std::runtime_error ("gs32 size != 0");
+  const gstring foo = C2GSTRING ("foo");
+  if (foo.needsFreeing()) throw std::runtime_error ("foo needsFreeing");
+  if (!foo.isReadOnly()) throw std::runtime_error ("foo !isReadOnly");
+  if (foo != "foo") throw std::runtime_error ("foo != foo");
+  if (foo.size() != 3) throw std::runtime_error ("foo not 3");
+  std::ostringstream oss; oss << gs16 << gsFree << gsRO << gs32 << foo;
+  if (oss.str() != "foo") throw std::runtime_error ("oss foo != foo");
+  glim::gstring_stream gss (gs16); std::ostream gsos (&gss);
+  gsos << "bar" << std::flush;
+  if (gs16 != "bar") throw std::runtime_error ("gs16 != bar");
+  gsos << "beer" << std::flush;
+  if (gs16 != "barbeer") throw std::runtime_error ("gs16 != barbeer");
+  gsos << "123456789" << std::flush;
+  if (gs16 != "barbeer123456789") throw std::runtime_error ("gs16 != barbeer123456789");
+  if (gs16.capacity() != 16) throw std::runtime_error ("gs16 != 16");
+  gsos << '0' << std::flush;
+  if (gs16 != "barbeer1234567890") throw std::runtime_error ("gs16 != barbeer1234567890");
+  if (gs16.capacity() != 32) throw std::runtime_error ("gs16 != 32");
+
+  gstring gsb; std::string str ("abc");
+  gsb << 'a' << 1 << 2LL << str;
+  std::string ns ("1:3,"); std::istringstream nsi (ns);
+  gsb.appendNetstring (nsi);
+  if (gsb != "a12abc3") throw std::runtime_error ("gsb != a12abc3");
+  if (strcmp (gsb.c_str(), "a12abc3") != 0) throw std::runtime_error ("strcmp ! 0");
+
+  std::cout << "pass." << std::endl;
+  return 0;
+}
