331391181506c727deb8800b9417e5fc27e4ada7
Artem <ArtemGr@users.noreply.github.com>
2012-12-16 22:52:25 +0000

    curl: gstring->string for the outgoing payload

--- curl.hpp
+++ curl.hpp
@@ -16,7 +16,7 @@ inline size_t curlWriteToGstring (void *buffer, size_t size, size_t nmemb, void 
   ((gstring*) userp)->append ((const char*) buffer, size * nmemb);
   return size * nmemb;};
 
-inline size_t curlReadFromGstring (void *ptr, size_t size, size_t nmemb, void *userdata);
+inline size_t curlReadFromString (void *ptr, size_t size, size_t nmemb, void *userdata);
 
 //inline size_t curlWriteToString (void *buffer, size_t size, size_t nmemb, void *userp) {
 //  ((std::string*) userp)->append ((const char*) buffer, size * nmemb);
@@ -29,7 +29,7 @@ inline size_t curlReadFromGstring (void *ptr, size_t size, size_t nmemb, void *u
  */
 class Curl {
  protected:
-  Curl (const Curl&): _curl (NULL), _headers (NULL), _send (NULL), _sent (0), _needs_cleanup (true) {} // No copying.
+  Curl (const Curl&): _curl (NULL), _headers (NULL), _sent (0), _needs_cleanup (true) {} // No copying.
  public:
   struct PerformError: std::runtime_error {
     char _message[CURL_ERROR_SIZE+1];
@@ -45,29 +45,27 @@ class Curl {
  public:
   CURL* _curl;
   struct curl_slist *_headers;
-  gstring* _send;
-  gstring _localSend;
+  std::string _send; ///< We're using `std::string` instead of `gstring` in order to support payloads larger than 16 MiB.
   uint32_t _sent;
   gstring _got;
   bool _needs_cleanup:1; ///< ~Curl will do `curl_easy_cleanup` if `true`.
   char _errorBuf[CURL_ERROR_SIZE];
 
   /** @param cleanup can be turned off if the CURL is freed elsewhere. */
-  Curl (bool cleanup = true): _curl (curl_easy_init()), _headers (NULL), _send (NULL), _sent (0), _needs_cleanup (cleanup) {*_errorBuf = 0;}
+  Curl (bool cleanup = true): _curl (curl_easy_init()), _headers (NULL), _sent (0), _needs_cleanup (cleanup) {*_errorBuf = 0;}
   /** Tries to reuse the given character buffer.
    * @param cleanup can be turned off if the CURL is freed elsewhere. */
-  Curl (size_t bufSize, char* buf, bool cleanup = true): _curl (curl_easy_init()), _headers (NULL), _send (NULL), _sent (0),
+  Curl (size_t bufSize, char* buf, bool cleanup = true): _curl (curl_easy_init()), _headers (NULL), _sent (0),
     _got (bufSize, buf, false, 0), _needs_cleanup (cleanup) {*_errorBuf = 0;}
   /** Wraps an existing handle (will invoke `curl_easy_cleanup` nevertheless).
    * @param cleanup can be turned off if the CURL is freed elsewhere. */
-  Curl (CURL* curl, bool cleanup = true): _curl (curl), _headers (NULL), _send (NULL), _sent (0), _needs_cleanup (cleanup) {*_errorBuf = 0;}
+  Curl (CURL* curl, bool cleanup = true): _curl (curl), _headers (NULL), _sent (0), _needs_cleanup (cleanup) {*_errorBuf = 0;}
   ~Curl(){
     if (_headers) {curl_slist_free_all (_headers); _headers = NULL;}
     if (_curl) {if (_needs_cleanup) curl_easy_cleanup (_curl); _curl = NULL;}
   }
 
-  Curl& send (gstring* text) {_send = text; _sent = 0; return *this;}
-  Curl& send (const gstring& text) {_localSend = text; _send = &_localSend; _sent = 0; return *this;}
+  template<typename STR> Curl& send (STR&& text) {_send = std::forward<std::string> (text); _sent = 0; return *this;}
 
   /** Adds "Content-Type" header into `_headers`. */
   Curl& contentType (const char* ct) {
@@ -89,10 +87,10 @@ class Curl {
     curl_easy_setopt (_curl, CURLOPT_NOSIGNAL, 1L); // required per http://curl.haxx.se/libcurl/c/libcurl-tutorial.html#Multi-threading
     curl_easy_setopt (_curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP);
     curl_easy_setopt (_curl, CURLOPT_ERRORBUFFER, _errorBuf);
-    if (_send && !_send->empty()) {
+    if (_send.size()) {
       curl_easy_setopt (_curl, CURLOPT_UPLOAD, 1L); // http://curl.haxx.se/libcurl/c/curl_easy_setopt.html#CURLOPTUPLOAD
-      curl_easy_setopt (_curl, CURLOPT_INFILESIZE, (long) _send->size());
-      curl_easy_setopt (_curl, CURLOPT_READFUNCTION, curlReadFromGstring);
+      curl_easy_setopt (_curl, CURLOPT_INFILESIZE, (long) _send.size());
+      curl_easy_setopt (_curl, CURLOPT_READFUNCTION, curlReadFromString);
       curl_easy_setopt (_curl, CURLOPT_READDATA, this);}
     if (_headers)
       curl_easy_setopt (_curl, CURLOPT_HTTPHEADER, _headers); // http://curl.haxx.se/libcurl/c/curl_easy_setopt.html#CURLOPTHTTPHEADER
@@ -110,8 +108,8 @@ class Curl {
     if (from) curl_easy_setopt (_curl, CURLOPT_MAIL_FROM, from);
     if (to) _headers = curl_slist_append (_headers, to);
     if (_headers) curl_easy_setopt (_curl, CURLOPT_MAIL_RCPT, _headers);
-    if (_send && !_send->empty()) {
-      curl_easy_setopt (_curl, CURLOPT_READFUNCTION, glim::curlReadFromGstring);
+    if (_send.size()) {
+      curl_easy_setopt (_curl, CURLOPT_READFUNCTION, glim::curlReadFromString);
       curl_easy_setopt (_curl, CURLOPT_READDATA, this);
     }
     return *this;
@@ -141,10 +139,10 @@ class Curl {
 
 };
 
-inline size_t curlReadFromGstring (void *ptr, size_t size, size_t nmemb, void *userdata) {
+inline size_t curlReadFromString (void *ptr, size_t size, size_t nmemb, void *userdata) {
   Curl* curl = (Curl*) userdata;
-  size_t len = std::min (curl->_send->size() - curl->_sent, size * nmemb);
-  if (len) memcpy (ptr, curl->_send->data() + curl->_sent, len);
+  size_t len = std::min (curl->_send.size() - curl->_sent, size * nmemb);
+  if (len) memcpy (ptr, curl->_send.data() + curl->_sent, len);
   curl->_sent += len;
   return len;}
 
