a558201e2fe4b7b8e86ea6fc6db5f084a34de852
Artemciy <Artemciy@27d5dcea-8f82-9947-98a7-4dea8f13ee8d>
2012-12-31 10:37:38 +0000

    exception: CAPTURE_TRACE

--- trunk/test_exception.cc
+++ trunk/test_exception.cc
@@ -1,4 +1,3 @@
-#define _GLIM_EXCEPTIONS_CODE
 #define _GLIM_ALL_EXCEPTIONS_CODE
 #include "exception.hpp"
 #include <iostream>
@@ -20,7 +19,7 @@ static void testThrowLine() {
   } catch (const std::exception& ex) {
     message = ex.what(); name = typeid (ex) .name();
   }
-  //std::cout << message << ' ' << name << ' ' << std::flush;
+  //std::cout << "testThrowLine: " << message << ' ' << name << ' ' << std::flush;
   assert (message.size());
   assert (std::string (message) .find (":" + std::to_string (line)) != std::string::npos);
   assert (name.find ("FooEx") != std::string::npos);
@@ -32,21 +31,39 @@ static void testThrowLine() {
     message = ex.what();
   }
   assert (message == "bar");
+  assert (glim::Exception::options() == 0);
+}
+
+static void testBacktrace() {
+  assert (glim::Exception::options() == 0);
+  glim::ExceptionControl captureTrace (glim::Exception::Options::CAPTURE_TRACE);
+  assert (glim::Exception::options() != 0);
+  std::string message;
+  try {
+    GTHROW ("message");
+  } catch (const std::exception& ex) {
+    message = ex.what();
+  }
+  //std::cout << "testBacktrace: " << message << std::endl;
+  assert (message.find ("[at bin/test_exception") != std::string::npos);
 }
 
 static void testAllExceptionsHack() {
+  assert (glim::Exception::options() == 0);
   std::string traceBuf;
-  glim::ExceptionHandler traceExceptions (glim::Exception::Options::HANDLE_ALL, glim::ExceptionHandler::backtrace, &traceBuf);
+  glim::ExceptionHandler traceExceptions (glim::Exception::Options::HANDLE_ALL, glim::captureBacktrace, &traceBuf);
+  assert (glim::Exception::options() != 0);
   try {
     throw "catch me"; // Catched by `_GLIM_ALL_EXCEPTIONS_CODE` and handled with `glim::ExceptionControl::backtrace`.
   } catch (const char* skip) {}
-  std::cout << std::endl << traceBuf << std::endl;
+  //std::cout << "testAllExceptionsHack: " << std::endl << traceBuf << std::endl;
   assert (traceBuf.size());
 }
 
 int main () {
   std::cout << "Testing exception.hpp ... " << std::flush;
   testThrowLine();
+  testBacktrace();
   testAllExceptionsHack();
   std::cout << "pass." << std::endl;
   return 0;
