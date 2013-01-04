3f08bc5ce46a0382a21cd7f5922b19faa45053f1
Artemciy <Artemciy@27d5dcea-8f82-9947-98a7-4dea8f13ee8d>
2013-01-04 22:37:22 +0000

    ldb: startsWith

--- trunk/ldb.hpp
+++ trunk/ldb.hpp
@@ -23,6 +23,7 @@ limitations under the License.
 
 #include <string>
 #include <unordered_map>
+#include <climits> // CHAR_MAX
 
 #include <leveldb/db.h>
 #include <leveldb/write_batch.h>
@@ -31,13 +32,14 @@ limitations under the License.
 #include <boost/serialization/serialization.hpp>
 #include <boost/noncopyable.hpp>
 #include <boost/iterator/iterator_facade.hpp>
-#include <boost/range/iterator_range.hpp>
+#include <boost/range/iterator_range.hpp> // http://www.boost.org/doc/libs/1_52_0/libs/range/doc/html/range/reference/utilities/iterator_range.html
 
 #include <arpa/inet.h> // htonl, ntohl
 #include <sys/stat.h> // mkdir
 #include <sys/types.h> // mkdir
 #include <string.h> // strerror
 #include <errno.h>
+#include <sstream>
 
 #include "gstring.hpp"
 #include "exception.hpp"
@@ -87,14 +89,21 @@ template <> inline void ldbDeserialize<std::string> (const gstring& bytes, std::
 struct Ldb {
   std::shared_ptr<leveldb::DB> _db;
 
-  struct IteratorEntry {
-    leveldb::Iterator* _lit;
+  struct IteratorEntry { // Something to be `dereference`d from the Iterator.
+    std::shared_ptr<leveldb::Iterator> _lit; // Iterator might be copied around, therefore we keep the real iterator in shared_ptr.
+    bool _valid:1;
+    IteratorEntry(): _valid (false) {}
+    IteratorEntry (leveldb::Iterator* lit, bool valid = false): _lit (lit), _valid (valid) {}
+    IteratorEntry (const std::shared_ptr<leveldb::Iterator>& lit, bool valid = false): _lit (lit), _valid (valid) {}
+    IteratorEntry (std::shared_ptr<leveldb::Iterator>&& lit, bool valid = false): _lit (std::move (lit)), _valid (valid) {}
     /** Zero-copy view of the current key bytes. Should *not* be used after the Iterator is changed or destroyed. */
     const gstring keyView() const {
+      if (!_valid) return gstring();
       const leveldb::Slice& key = _lit->key();
       return gstring (0, (void*) key.data(), false, key.size(), true);} // Zero copy.
     /** Zero-copy view of the current value bytes. Should *not* be used after the Iterator is changed or destroyed. */
     const gstring valueView() const {
+      if (!_valid) return gstring();
       const leveldb::Slice& val = _lit->value();
       return gstring (0, (void*) val.data(), false, val.size(), true);} // Zero copy.
     /** Deserialize into `key`. */
@@ -108,51 +117,98 @@ struct Ldb {
   };
   /** Wraps Leveldb iterator. */
   struct Iterator: public boost::iterator_facade<Iterator, IteratorEntry, boost::bidirectional_traversal_tag> {
-    std::shared_ptr<leveldb::Iterator> _lit; // Iterator might be copied around, therefore we keep the real iterator in shared_ptr.
     IteratorEntry _entry;
 
     Iterator (const Iterator&) = default;
     Iterator (Iterator&&) = default;
     /** Iterate from the beginning or the end of the database.
      * @param position can be MDB_FIRST or MDB_LAST */
-    Iterator (Ldb* ldb, leveldb::ReadOptions options = leveldb::ReadOptions()): _lit (ldb->_db->NewIterator (options)) {
-      _lit->SeekToFirst();
+    Iterator (Ldb* ldb, leveldb::ReadOptions options = leveldb::ReadOptions()): _entry (ldb->_db->NewIterator (options)) {
+      _entry._lit->SeekToFirst();
+      _entry._valid = _entry._lit->Valid();
     }
 
-    struct EndIteratorFlag {};
-    Iterator (Ldb* ldb, EndIteratorFlag, leveldb::ReadOptions options = leveldb::ReadOptions()): _lit (ldb->_db->NewIterator (options)) {}
+    struct NoSeekFlag {}; ///< Tells `Iterator` constructor not to seek to the beginning of the database.
+    Iterator (Ldb* ldb, NoSeekFlag, leveldb::ReadOptions options = leveldb::ReadOptions()): _entry (ldb->_db->NewIterator (options)) {}
     /** True if the iterator isn't pointing anywhere. */
-    bool end() const {return !_lit->Valid();}
+    bool end() const {return !_entry._valid;}
 
-    Iterator (const std::shared_ptr<leveldb::Iterator>& lit): _lit (lit) {}
-    Iterator (std::shared_ptr<leveldb::Iterator>&& lit): _lit (std::move (lit)) {}
+    Iterator (const std::shared_ptr<leveldb::Iterator>& lit): _entry (lit) {}
+    Iterator (std::shared_ptr<leveldb::Iterator>&& lit): _entry (std::move (lit)) {}
 
     bool equal (const Iterator& other) const {
-      bool weAreValid = _lit->Valid(), theyAreValid = other->_lit->Valid();
+      bool weAreValid = _entry._valid, theyAreValid = other._entry._valid;
       if (!weAreValid) return !theyAreValid;
       if (!theyAreValid) return false;
-      auto&& ourKey = _lit->key(), theirKey = other->_lit->key();
+      auto&& ourKey = _entry._lit->key(), theirKey = other._entry._lit->key();
       if (ourKey.size() != theirKey.size()) return false;
       return memcmp (ourKey.data(), theirKey.data(), ourKey.size()) == 0;
     }
     IteratorEntry& dereference() const {
-      IteratorEntry& entry = const_cast<IteratorEntry&> (_entry);
-      entry._lit = _lit.get();
       // NB: Boost iterator_facade expects the `dereference` to be a `const` method.
       //     I guess Iterator is not modified, so the `dereference` is `const`, even though the Entry can be modified.
-      return entry;
+      return const_cast<IteratorEntry&> (_entry);
     }
-    void increment() {
-      if (_lit->Valid()) _lit->Next();
-      else _lit->SeekToFirst();
+    virtual void increment() {
+      if (_entry._valid) _entry._lit->Next();
+      else _entry._lit->SeekToFirst();
+      _entry._valid = _entry._lit->Valid();
     }
-    void decrement() {
-      if (_lit->Valid()) _lit->Prev();
-      else _lit->SeekToLast();
+    virtual void decrement() {
+      if (_entry._valid) _entry._lit->Prev();
+      else _entry._lit->SeekToLast();
+      _entry._valid = _entry._lit->Valid();
     }
   };
   Iterator begin() {return Iterator (this);}
-  const Iterator end() {return Iterator (this, Iterator::EndIteratorFlag());}
+  const Iterator end() {return Iterator (this, Iterator::NoSeekFlag());}
+
+  struct StartsWithIterator: public Iterator {
+    gstring _starts;
+    StartsWithIterator (const StartsWithIterator&) = default;
+    StartsWithIterator (StartsWithIterator&&) = default;
+    /** End iterator, pointing nowhere. */
+    StartsWithIterator (Ldb* ldb): Iterator (ldb, NoSeekFlag()) {}
+    StartsWithIterator (Ldb* ldb, const char* data, uint32_t length, leveldb::ReadOptions options = leveldb::ReadOptions()):
+      Iterator (ldb, NoSeekFlag(), options), _starts (data, length) {
+      _entry._lit->Seek (leveldb::Slice (data, length));
+      _entry._valid = checkValidity();
+    }
+    bool checkValidity() const {
+      return _entry._lit->Valid() && _entry._lit->key().starts_with (leveldb::Slice (_starts.data(), _starts.length()));
+    }
+    virtual void increment() override {
+      if (_entry._valid) _entry._lit->Next();
+      else _entry._lit->Seek (leveldb::Slice (_starts.data(), _starts.length()));
+      _entry._valid = checkValidity();
+    }
+    virtual void decrement() override {
+      if (_entry._valid) _entry._lit->Prev(); else seekLast();
+      _entry._valid = checkValidity();
+    }
+    void seekLast() {
+      leveldb::Iterator* lit = _entry._lit.get();
+      // Go somewhere *below* the `_starts` prefix.
+      char after[_starts.length()]; memcpy (after, _starts.data(), sizeof (after));
+      uint32_t pos = sizeof (after); while (--pos >= 0) if (after[pos] < CHAR_MAX) {++after[pos]; break;}
+      if (pos >= 0) {lit->Seek (leveldb::Slice (after, sizeof (after))); if (!lit->Valid()) lit->SeekToLast();} else lit->SeekToLast();
+      // Seek back until we are in the `_starts` prefix.
+      for (leveldb::Slice prefix (_starts.data(), _starts.length()); lit->Valid(); lit->Prev()) {
+        leveldb::Slice key (lit->key());
+        if (key.starts_with (prefix)) break; // We're "back" in the `_starts` prefix.
+        if (key.compare (prefix) < 0) break; // Gone too far (no prefix entries in the db).
+      }
+    }
+  };
+
+  /** Range over entries starting with `key`. */
+  template <typename K>
+  boost::iterator_range<StartsWithIterator> startsWith (const K& key) {
+    char kbuf[64]; // Allow up to 64 bytes to be serialized without heap allocations.
+    gstring kbytes (sizeof (kbuf), kbuf, false, 0);
+    ldbSerialize (kbytes, key);
+    return boost::iterator_range<StartsWithIterator> (StartsWithIterator (this, kbytes.data(), kbytes.length()), StartsWithIterator (this));
+  }
 
   struct Trigger {
     virtual gstring triggerName() const {return C2GSTRING ("defaultTriggerName");};
