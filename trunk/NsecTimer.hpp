46d29e81e3bea25e4ac12b06907a343e57b523f8
Artemciy <Artemciy@27d5dcea-8f82-9947-98a7-4dea8f13ee8d>
2013-05-29 06:35:57 +0000

    NsecTimer: an option to get the time in a `double`

--- trunk/NsecTimer.hpp
+++ trunk/NsecTimer.hpp
@@ -17,10 +17,17 @@ struct NsecTimer {
     timespec nsecStop; clock_gettime (CLOCK_MONOTONIC, &nsecStop);
     return (int64_t) (nsecStop.tv_sec - start.tv_sec) * 1000000000LL + (int64_t) (nsecStop.tv_nsec - start.tv_nsec);
   }
+  /** Seconds since the creation or restart of the timer. */
+  double sec() const {
+    timespec nsecStop; clock_gettime (CLOCK_MONOTONIC, &nsecStop);
+    double seconds = nsecStop.tv_sec - start.tv_sec;
+    seconds += (double)(nsecStop.tv_nsec - start.tv_nsec) / 1000000000.0;
+    return seconds;
+  }
   //! Seconds since the creation or restart of the timer.
   std::string seconds (int precision = 9) const {
     // The trick is to avoid the scientific notation by printing integers.
-    double sec = operator()() / 1000000000.0;
+    double sec = this->sec();
     std::ostringstream buf;
     int isec = (int) sec;
     buf << isec;
