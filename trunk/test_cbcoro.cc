cf9b491c79fe5eca677000ac24b7bc66dd3b659b
Artemciy <Artemciy@27d5dcea-8f82-9947-98a7-4dea8f13ee8d>
2013-07-11 16:04:04 +0000

    cbcoro: Automatic memory management (CBCoroPtr)

--- trunk/test_cbcoro.cc
+++ trunk/test_cbcoro.cc
@@ -28,13 +28,13 @@ void esDelete (int frople, std::function<void(int)> cb) {
   }); th.detach();
 }
 
-struct RemoveFroples: public CBCoro<4096> {
+struct RemoveFroples: public glim::CBCoro<4096> {
   const char* _argument;
   RemoveFroples (const char* argument): _argument (argument) {
     printf ("RF: constructor\n");
-    cbcStart();
   }
-  virtual void run() throw() override {
+  virtual ~RemoveFroples() {puts ("~RemoveFroples");}
+  virtual void run() override {
     for (int i = 1; i <= 4; ++i) {
       printf ("RF: Removing frople %i...\n", i);
       int returnedFrople = 0;
@@ -54,16 +54,13 @@ struct RemoveFroples: public CBCoro<4096> {
       });
       printf ("RF: Returned from callback; _returnTo is: %li; frople %i\n", (intptr_t) _returnTo, returnedFrople);
     }
-    deleteLater (this);
     printf ("RF: finish! _returnTo is: %li\n", (intptr_t) _returnTo);
-    cbcReturn();
   };
 };
 
 int main() {
-  new RemoveFroples ("argument");
+  (new RemoveFroples ("argument"))->start();
   printf ("main: returned from RemoveFroples\n");
   sleep (5);
-  CBCoroStatic::maintance(); // Perform any pending cleanups.
   return 0;
 }
