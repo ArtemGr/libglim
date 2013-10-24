f2e8810c8c7aa87cdbd5a7d25f082b6771dfec6b
Artem <ArtemGr@users.noreply.github.com>
2013-10-24 21:26:43 +0000

    raii: fix the `finally` to actually work

--- raii.hpp
+++ raii.hpp
@@ -4,32 +4,31 @@ namespace glim {
 // http://stackoverflow.com/questions/2121607/any-raii-template-in-boost-or-c0x/
 
 /// RAII helper. Keeps the functor and runs it in the destructor.
-/// The idea to name it `finally` comes from http://www.codeproject.com/Tips/476970/finally-clause-in-Cplusplus.
-/// Example: \code
-///   finally unmap ([&]() {munmap (fd, size);});
-///   // or
-///   auto unmap = raiiFun ([&]() {munmap (fd, size);});
-/// \endcode
-template<typename Fun> struct finally {
+/// Example: \code auto unmap = raiiFun ([&]() {munmap (fd, size);}); \endcode
+template<typename Fun> struct RAIIFun {
   Fun _fun;
-  finally (finally&&) = default;
-  finally (const finally&) = default;
-  template<typename FunArg> finally (FunArg fun): _fun (std::forward<Fun> (fun)) {}
-  ~finally() {_fun();}
+  RAIIFun (RAIIFun&&) = default;
+  RAIIFun (const RAIIFun&) = default;
+  template<typename FunArg> RAIIFun (FunArg fun): _fun (std::forward<Fun> (fun)) {}
+  ~RAIIFun() {_fun();}
 };
 
-/// Runs the given functor when going out of scope.
-/// Example: \code
-///   auto closeFd = raiiFun ([&]() {close (fd);});
-///   auto unmap = raiiFun ([&]() {munmap (fd, size);});
-/// \endcode
-template<typename Fun> finally<Fun> raiiFun (const Fun& fun) {return finally<Fun> (fun);}
+/// The idea to name it `finally` comes from http://www.codeproject.com/Tips/476970/finally-clause-in-Cplusplus.
+/// Example: \code finally unmap ([&]() {munmap (fd, size);}); \endcode
+typedef RAIIFun<std::function<void(void)>> finally;
 
 /// Runs the given functor when going out of scope.
 /// Example: \code
 ///   auto closeFd = raiiFun ([&]() {close (fd);});
 ///   auto unmap = raiiFun ([&]() {munmap (fd, size);});
 /// \endcode
-template<typename Fun> finally<Fun> raiiFun (Fun&& fun) {return finally<Fun> (std::move (fun));}
+template<typename Fun> RAIIFun<Fun> raiiFun (const Fun& fun) {return RAIIFun<Fun> (fun);}
+
+/// Runs the given functor when going out of scope.
+/// Example: \code
+///   auto closeFd = raiiFun ([&]() {close (fd);});
+///   auto unmap = raiiFun ([&]() {munmap (fd, size);});
+/// \endcode
+template<typename Fun> RAIIFun<Fun> raiiFun (Fun&& fun) {return RAIIFun<Fun> (std::move (fun));}
 
 }
