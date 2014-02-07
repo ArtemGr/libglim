fa4e79785579d7cc5cbb91c00cd6821c18bfc0ac
Artem <ArtemGr@users.noreply.github.com>
2014-02-07 13:04:38 +0000

    curl: Helpers to gather verbose debugging from cURL.

--- test_runner.cc
+++ test_runner.cc
@@ -14,10 +14,12 @@ int main () {
     runner.schedule (0.f, [=](glim::Runner::JobInfo&)->bool {*scheduledJobFired = true; return false;});
 
     auto curl = std::make_shared<glim::Curl> (false); curl->http ("http://glim.ru/env.cgi?pause=50", 5);
+    auto curlDebug = std::make_shared<std::string>(); curl->debugListenerF ([curlDebug](const char* bytes, size_t size) {curlDebug->append (bytes, size);});
     volatile bool ran = false;
-    runner.multi (curl->_curl, [curl,&ran,evbase](CURLMsg* msg) {
+    runner.multi (curl->_curl, [curl,&ran,evbase,curlDebug](CURLMsg* msg) {
       std::cout << " status: " << curl->status();
       if (curl->status() == 200) std::cout << " ip: " << curl->gstr().view (0, std::max (curl->gstr().find ("\n"), 0));
+      if (curlDebug->find ("GET /env.cgi") == std::string::npos) std::cerr << " No headers in debug? " << *curlDebug << std::endl;
       ran = true;
       event_base_loopbreak (evbase.get());
     });
